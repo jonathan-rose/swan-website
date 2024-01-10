a78bb708f54e2af2ca8d8126e27467d5dcd931a8
Jonathan Rose <jonathan_rose@outlook.com>
2024-01-10 18:38:00 +0000

    Centered gallery images and arrows have consistent position.

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -4,27 +4,10 @@ import ReactImageGallery from 'react-image-gallery';
 const images = require.context('../../../public/images/gallery', true)
 const imageList = images.keys().map(image => ({ original: images(image) }));
 
-console.log(imageList);
-
-const images2 = [
-  {
-    original: "https://picsum.photos/id/1018/1000/600/",
-    thumbnail: "https://picsum.photos/id/1018/250/150/",
-  },
-  {
-    original: "https://picsum.photos/id/1015/1000/600/",
-    thumbnail: "https://picsum.photos/id/1015/250/150/",
-  },
-  {
-    original: "https://picsum.photos/id/1019/1000/600/",
-    thumbnail: "https://picsum.photos/id/1019/250/150/",
-  },
-];
-
-console.log(images2);
-
 export default function Gallery() {
   return (
-    <ReactImageGallery items={imageList} />
+    <ReactImageGallery 
+    items={imageList}
+    slideDuration={0} />
   )
 }
