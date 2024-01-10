d54bf112b472bf6a7a9f71380533618a881a1996
Jonathan Rose <jonathan_rose@outlook.com>
2024-01-10 17:49:26 +0000

    Gallery added using react-image-gallery.

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,7 +1,30 @@
-import './gallery.css'
+import './gallery.css';
+import ReactImageGallery from 'react-image-gallery';
+
+const images = require.context('../../../public/images/gallery', true)
+const imageList = images.keys().map(image => ({ original: images(image) }));
+
+console.log(imageList);
+
+const images2 = [
+  {
+    original: "https://picsum.photos/id/1018/1000/600/",
+    thumbnail: "https://picsum.photos/id/1018/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1015/1000/600/",
+    thumbnail: "https://picsum.photos/id/1015/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1019/1000/600/",
+    thumbnail: "https://picsum.photos/id/1019/250/150/",
+  },
+];
+
+console.log(images2);
 
 export default function Gallery() {
   return (
-    <div>Gallery</div>
+    <ReactImageGallery items={imageList} />
   )
 }
