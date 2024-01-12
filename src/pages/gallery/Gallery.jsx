5fdeb8b5711cc514ba59053c22941c46e8e8f162
Jonathan Rose <jonathan_rose@outlook.com>
2024-01-12 06:45:59 +0000

    Restrict width and height of gallery images.

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -2,7 +2,7 @@ import './gallery.css';
 import ReactImageGallery from 'react-image-gallery';
 
 const images = require.context('../../../public/images/gallery', true)
-const imageList = images.keys().map(image => ({ original: images(image) }));
+const imageList = images.keys().map(image => ({ original: images(image), thumbnail: (images(image))}));
 
 export default function Gallery() {
   return (
