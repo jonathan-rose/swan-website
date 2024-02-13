1343c770e01cf6b792df216689683982c372ab9c
Jonathan Rose <jonathan_rose@outlook.com>
2024-02-13 08:01:42 +0000

    Adding About link on front page.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import './home.css'
 import Logo from '../../components/logo/Logo'
 
@@ -13,10 +14,16 @@ export default function Home() {
     <>
       <div className="home">
         <div className="homeBanner">
-          <img src="../../images/content/park1.jpg"/>
+          <img id="banner-image" src="../../images/content/park1.jpg"/>
           <div class="heading-container">
+            <h1 id="swan">We are SWAN.</h1>
             <h1 id="work"> We work with children, young people and families.</h1>
             <h1 id="impact"> Together we can have a bigger impact.</h1>
+          </div>
+          <div class="learn-more">
+          <Link className="link" to="/about">
+              Learn more about us...
+            </Link>
           </div>
         </div>
       </div>
