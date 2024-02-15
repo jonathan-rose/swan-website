90572b4a350468c8b48aad2fed6a94698ed04b8e
Jonathan Rose <jonathan_rose@outlook.com>
2024-02-15 18:06:25 +0000

    Improved column layout on smaller screen sizes.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -13,19 +13,19 @@ export default function Home() {
   return (
     <>
       <div className="home">
-        <div className="homeBanner">
-          <img id="banner-image" src="../../images/content/park1.jpg"/>
-          <div class="heading-container">
+        <div className="content">
+          <div className="heading-container">
             <h1 id="swan">We are SWAN.</h1>
             <h1 id="work"> We work with children, young people and families.</h1>
             <h1 id="impact"> Together we can have a bigger impact.</h1>
           </div>
-          <div class="learn-more">
-          <Link className="link" to="/about">
+          <div className="learn-more">
+            <Link className="link" to="/about">
               Learn more about us...
             </Link>
           </div>
         </div>
+        <img id="banner-image" src="../../images/content/park1.jpg" />
       </div>
     </>
   );
