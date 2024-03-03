cee9a3e593576bdc14e06e1af65c244f93081319
Jonathan Rose <jonathan_rose@outlook.com>
2024-03-03 20:17:19 +0000

    Added basic Info and Map sections.

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -2,6 +2,19 @@ import './contact.css'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div class="container">
+      <section id="contact-info">
+        <h2>Info</h2>
+        <ul>
+          <li>Phone:</li>
+          <li>Email:</li>
+          <li>Address:</li>
+        </ul>
+      </section>
+      <section id="map">
+        <h2>Location</h2>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2492.8532092287664!2d-2.96828892313459!3d51.332219723485466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4871f8c7dbb4decd%3A0xcdf90b4376839da1!2sFor%20All%20Healthy%20Living%20Centre!5e0!3m2!1sen!2suk!4v1709495151720!5m2!1sen!2suk" width="80%" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </section>
+    </div>
   )
 }
