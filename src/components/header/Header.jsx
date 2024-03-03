1401d1ff8a5900fd2fc765ae91e12234110c313d
Jonathan Rose <40912659+jonathan-rose@users.noreply.github.com>
2024-03-03 18:05:47 +0000

    Revert "Video demo"

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,52 +1,12 @@
-import { Link } from "react-router-dom"
 import "./header.css"
-import Logo from "../logo/Logo"
-import SocialBtn from "../socialbtn/SocialBtn"
 
 export default function Header() {
   return (
-    <div className='header' id='header'>
-      <div className="topLeft">
-        <Logo></Logo>
-      </div>
-      <div className="topCenter">
-        <ul className="topList">
-          <li className="topListItem">
-            <Link className="link" to="/">
-              Home
-            </Link>
-          </li>
-          {/* <li className="topListItem">
-            <Link className="link" to="/about">
-              About
-            </Link>
-          </li> */}
-          <li className="topListItem">
-            <Link className="link" to="/video">
-              What We Do
-            </Link>
-          </li>
-          {/* <li className="topListItem">
-            <Link className="link" to="/gallery">
-              Gallery
-            </Link>
-          </li>
-          <li className="topListItem">
-            <Link className="link" to="/contact">
-              Contact
-            </Link>
-          </li> */}
-        </ul>
-      </div>
-      <div className="topRight">
-      <SocialBtn 
-          faIcon="facebook"
-          faSize="5x"
-          link="https:/www.facebook.com/SWAN.WSM/"
-          displayText="Facebook"
-          showText={false}
-        />
-      </div>
+    <div className='header'>
+        <div className="headerTitles">
+          <span className="headerTitle">South West Activity Network</span>
+        </div>
+        <img className="headerImg" src="/images/weston1.jpg" alt="" />
     </div>
   )
 }
