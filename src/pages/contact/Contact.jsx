a9fb7775a6f581b7b305a60e735bde40772db5b1
Jonathan Rose <jonathan_rose@outlook.com>
2024-03-17 15:00:48 +0000

    Tweak to contact headings to give visual balance.

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -4,8 +4,10 @@ export default function Contact() {
   return (
     <div class="contact-container">
       <section class="contact-section" id="contact-info">
-        <h1 class="contact-header">Contact Us</h1>
         <div id="contact-items">
+          <span id="heading-span">
+            <h1 class="contact-heading">Contact Us</h1>
+          </span>
           <span class="contact-span">
             <i class="fa-solid fa-mobile-screen"></i>
             <p>01934 123 456</p>
@@ -26,9 +28,9 @@ export default function Contact() {
           </span>
         </div>
       </section>
-      <section class="contact-section" id="map">
-        <h1 class="contact-header">Visit Us</h1>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2492.8532092287664!2d-2.96828892313459!3d51.332219723485466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4871f8c7dbb4decd%3A0xcdf90b4376839da1!2sFor%20All%20Healthy%20Living%20Centre!5e0!3m2!1sen!2suk!4v1709495151720!5m2!1sen!2suk" width="70%" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <section class="contact-section" id="map-section">
+        <h1 class="contact-heading">Visit Us</h1>
+        <iframe id="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2492.8532092287664!2d-2.96828892313459!3d51.332219723485466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4871f8c7dbb4decd%3A0xcdf90b4376839da1!2sFor%20All%20Healthy%20Living%20Centre!5e0!3m2!1sen!2suk!4v1709495151720!5m2!1sen!2suk" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </section>
     </div>
   )
